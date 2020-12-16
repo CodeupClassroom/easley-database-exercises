e9ca72a526d06e13022cf8a32accbdb4a1ae25c3
RyanOrsinger <ryan.orsinger@gmail.com>
2020-12-16 10:43:29 -0600

    Add first exercise and readme

---
+++ mysql_introduction.sql
@@ -0,0 +1,11 @@
+# * means all columns from a database table
+SELECT * FROM mysql.user;
+
+# only showing the "user" column and the "host" columns
+SELECT user, host FROM mysql.user;
+
+# All the rows because we use * from the help_topic table of the mysql database
+SELECT * FROM mysql.help_topic;
+
+# Selecting only the help_topic_id, help_category_id, and url columns from the help_topic table.
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
